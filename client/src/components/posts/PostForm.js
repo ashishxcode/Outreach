739d54c5d82e222213942c8ea2f71dc829879dc3
ashishxcode <ashish.patel0903@gmail.com>
2021-08-01 00:24:49 +0530

    Created image upload button

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -1,37 +1,59 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { addPost } from "../../actions/post";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/post';
+import { FiImage } from 'react-icons/fi';
 
 const PostForm = ({ addPost }) => {
-  const [text, setText] = useState("");
+	const [text, setText] = useState('');
+	const [image, setImage] = useState(null);
 
-  return (
-    <div className='post-form'>
-      <form
-        className='form my-1'
-        onSubmit={(e) => {
-          e.preventDefault();
-          addPost({ text });
-          setText("");
-        }}>
-        <textarea
-          name='text'
-          cols='30'
-          rows='5'
-          placeholder='What is in your mind?'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          required
-        />
-        <input type='submit' className='btn btn-dark my-1' value='Submit' />
-      </form>
-    </div>
-  );
+	const handleImageUpload = (e) => {
+		console.log('Image upload clicked', e);
+	};
+
+	return (
+		<div className="post-form">
+			<form
+				className="form my-1"
+				onSubmit={(e) => {
+					e.preventDefault();
+					addPost({ text });
+					setText('');
+				}}>
+				<textarea
+					name="text"
+					cols="30"
+					rows="5"
+					placeholder="What is in your mind?"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+					required></textarea>
+
+				<div className="form-action  my-1">
+					<div className="image-upload">
+						<label for="file-input">
+							<FiImage className="upload-icon" />
+						</label>
+						<input
+							id="file-input"
+							type="file"
+							onChange={handleImageUpload}
+						/>
+					</div>
+					<input
+						type="submit"
+						className="btn btn-dark"
+						value="Submit"
+					/>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 PostForm.propTypes = {
-  addPost: PropTypes.func.isRequired,
+	addPost: PropTypes.func.isRequired,
 };
 
 export default connect(null, { addPost })(PostForm);
