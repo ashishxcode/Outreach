17a227a3bb99bd9bfcb27ef857d3ced4e926f02c
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-08-01 00:01:03 +0530

    deps installed : cloudinary

--- server.js
+++ server.js
@@ -29,4 +29,7 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+	// console.log("CLODUINARY KEY->", process.env.CLOUDINARY_CLOUD_NAME);
+});
