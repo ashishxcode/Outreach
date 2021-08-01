c77485d7495691726c00312fe4a29f3a11a6e5cf
ashishxcode <ashish.patel0903@gmail.com>
2021-08-01 13:22:09 +0530

    Fixed Array.length which rendring 0

--- client/src/components/posts/PostForm.js
+++ client/src/components/posts/PostForm.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { addPost } from "../../actions/post";
-import { FiImage } from "react-icons/fi";
-import Resizer from "react-image-file-resizer";
-import api from "../../utils/api";
-import { Badge } from "antd";
-import { AiOutlineDelete } from "react-icons/ai";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addPost } from '../../actions/post';
+import { FiImage } from 'react-icons/fi';
+import Resizer from 'react-image-file-resizer';
+import api from '../../utils/api';
+import { Badge } from 'antd';
+import { AiOutlineDelete } from 'react-icons/ai';
 
 const PostForm = ({ addPost }) => {
-	const [text, setText] = useState("");
+	const [text, setText] = useState('');
 	const [images, setImages] = useState([]);
 	// const [imageURL, setImageURL] = useState();
 	// const [imageId, setImageId] = useState();
@@ -28,26 +28,28 @@ const PostForm = ({ addPost }) => {
 					files[i],
 					200,
 					200,
-					"JPEG",
+					'JPEG',
 					100,
 					0,
 					(uri) => {
 						//the calback dunction : most important
-						console.log("uri ===> ", uri);
-						api
-							.post("http://localhost:5000/api/imagesUpload/", {
-								image: uri,
-							})
+						console.log('uri ===> ', uri);
+						api.post('http://localhost:5000/api/imagesUpload/', {
+							image: uri,
+						})
 							.then((res) => {
 								console.log(
-									"res from images ==>",
-									res.data.url
+									'res from images ==>',
+									res.data.url,
 									// setImageURL(res.data.url)
 								);
 
 								filesUploaded.push(res.data);
 								setImages(filesUploaded);
-								console.log("filesUploaded ===> ", filesUploaded);
+								console.log(
+									'filesUploaded ===> ',
+									filesUploaded,
+								);
 							})
 							.catch((err) => {
 								console.log(err.message);
@@ -55,22 +57,22 @@ const PostForm = ({ addPost }) => {
 
 						//make request to backend with images
 					},
-					"base64"
+					'base64',
 				);
 			}
 		}
 	};
 
 	const handleDeleteImage = (public_id) => {
-		console.log("delete click", public_id);
-		api
-			.post("http://localhost:5000/api/imagesUpload/removeImage", { public_id })
-			.then((res) => {
-				let filteredImages = images.filter((item) => {
-					return item.public_id !== public_id;
-				});
-				setImages(filteredImages);
+		console.log('delete click', public_id);
+		api.post('http://localhost:5000/api/imagesUpload/removeImage', {
+			public_id,
+		}).then((res) => {
+			let filteredImages = images.filter((item) => {
+				return item.public_id !== public_id;
 			});
+			setImages(filteredImages);
+		});
 	};
 	return (
 		<div className="post-form">
@@ -79,9 +81,8 @@ const PostForm = ({ addPost }) => {
 				onSubmit={(e) => {
 					e.preventDefault();
 					addPost({ text });
-					setText("");
-				}}
-			>
+					setText('');
+				}}>
 				<textarea
 					name="text"
 					cols="30"
@@ -91,23 +92,26 @@ const PostForm = ({ addPost }) => {
 					onChange={(e) => setText(e.target.value)}
 					required
 				/>
-				{images &&
-					images.length &&
-					images.map((image) => {
-						return (
-							<Badge
-								count="❌"
-								onClick={() => {
-									handleDeleteImage(image.public_id);
-								}}
-								offset={[0, 10]}
-								style={{ cursor: "pointer" }}
-								id={image.public_id}
-							>
-								<img src={image.url} className="image-thumbnail" alt="" />
-							</Badge>
-						);
-					})}
+				{images && images.length
+					? images.map((image) => {
+							return (
+								<Badge
+									count="❌"
+									onClick={() => {
+										handleDeleteImage(image.public_id);
+									}}
+									offset={[0, 10]}
+									style={{ cursor: 'pointer' }}
+									id={image.public_id}>
+									<img
+										src={image.url}
+										className="image-thumbnail"
+										alt=""
+									/>
+								</Badge>
+							);
+					  })
+					: ''}
 				<div className="form-action  my-1">
 					<div className="image-upload">
 						<label for="file-input">
@@ -120,7 +124,11 @@ const PostForm = ({ addPost }) => {
 							onChange={handleimageResizeAndUpload}
 						/>
 					</div>
-					<input type="submit" className="btn btn-dark" value="Submit" />
+					<input
+						type="submit"
+						className="btn btn-dark"
+						value="Submit"
+					/>
 				</div>
 			</form>
 		</div>
